fix bug in signal to informed travellers resulting in nans, changing it to using mean and standard deviation of experienced waiting time or income

MaaSSim/d2d_demand.py
@@ -171,8 +171,11 @@ def wom_trav(inData, end_day, **kwargs):
     params = kwargs.get('params', None)
     exp_inf_trav = end_day.loc[end_day.informed]
     average_xp_wait = exp_inf_trav.corr_xp_wait.mean() / 60
-    signal = (np.random.lognormal(params.evol.travellers.inform.mu_log, np.sqrt(2 * (np.log(average_xp_wait) - params.evol.travellers.inform.mu_log)), len(inData.passengers))) * 60
-    signal = np.where(signal <= 7200, signal, 7200)
+    std_xp_wait = exp_inf_trav.corr_xp_wait.std() / 60
+    log_mu = np.log(average_xp_wait ** 2 / math.sqrt(std_xp_wait ** 2 + average_xp_wait ** 2))
+    log_std = math.sqrt(np.log(std_xp_wait ** 2 / average_xp_wait ** 2 + 1))
+    signal = (np.random.lognormal(log_mu, log_std, len(inData.passengers))) * 60
+    signal = np.where(signal <= 3600, signal, 3600)
     nP_inf = inData.passengers.informed.sum()
     nP_uninf = len(inData.passengers) - nP_inf
 

MaaSSim/d2d_supply.py
@@ -141,6 +141,7 @@ def learning_unregist(inData, end_day, **kwargs):
     params = kwargs.get('params', None)
     exp_reg_drivers = end_day[end_day.registered]
     average_xp_income = exp_reg_drivers.exp_inc.mean()
+    std_xp_income = exp_reg_drivers.exp_inc.std()
 
     cond_new_inf = inData.vehicles.informed & ~inData.vehicles.registered & end_day.new_perc_inc.isna()
     cond_prev_inf = inData.vehicles.informed & ~inData.vehicles.registered & ~end_day.new_perc_inc.isna()
@@ -148,8 +149,7 @@ def learning_unregist(inData, end_day, **kwargs):
                             'cond_prev_inf': cond_prev_inf}, index=np.arange(1, params.nV + 1))
 
     if (~end_day.out).any(axis=0):  # at least a single participating driver
-        df['signal'] = np.random.normal(average_xp_income, params.evol.drivers.inform.signal_rel_std * average_xp_income,
-                              len(inData.vehicles))
+        df['signal'] = np.random.normal(average_xp_income, std_xp_income, len(inData.vehicles))
         df['perc_inc'] = end_day.new_perc_inc * (1 - params.evol.drivers.kappa) + df.signal * params.evol.drivers.kappa
     else:
         df['signal'] = np.ones(len(inData.vehicles)) * end_day.new_perc_inc.mean()