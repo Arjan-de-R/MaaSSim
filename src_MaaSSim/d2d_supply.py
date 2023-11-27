11e7975986e427f0f175bbff4538977dbbb35b9c
Arjan de Ruijter <arjan_de_ruijter@hotmail.com>
2023-11-27 10:55:15 +0100

    update function to include params

--- src_MaaSSim/d2d_supply.py
+++ src_MaaSSim/d2d_supply.py
@@ -33,7 +33,7 @@ def generate_vehicles_d2d(_inData, _params=None):
     rand_informed = np.random.rand(_params.nV) < _params.evol.drivers.inform.prob_start
     vehs['informed'] = rand_informed
     vehs['registered'] = (np.random.rand(_params.nV) < _params.evol.drivers.regist.prob_start) * rand_informed
-    vehs['registered'] = vehs.apply(lambda row: (row.registered * np.full(len(_inData.platforms.index),True)) if row.multihoming else sh_init_reg(row.registered * np.full(len(_inData.platforms.index),True)), axis=1) # multi-homers, else single-homers
+    vehs['registered'] = vehs.apply(lambda row: (row.registered * np.full(len(_inData.platforms.index),True)) if row.multihoming else sh_init_reg(_params, row.registered * np.full(len(_inData.platforms.index),True)), axis=1) # multi-homers, else single-homers
     if _params.evol.drivers.get('start_perc_inc_avg_ratio', False):
         vehs['expected_income'] = vehs.apply(lambda row: zero_to_nan(row.registered * _params.evol.drivers.start_perc_inc_avg_ratio * vehs.res_wage.mean()), axis=1)  # everyone expects (factor of) mean reservation wage in population
     else:
